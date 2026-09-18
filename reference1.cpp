a5fad221beb1613fac40ec58a968ab6c58bdb92d
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-18 09:44:29 +0700

    Reference practice 1

---
+++ reference1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    int x = 10;
+    int& rx = x;    //reference to x
+    rx = 20;        // change value using reference
+    cout << "x = " << x << endl;
+    cout << "rx = " << rx << endl;
+    return 0;
+}
