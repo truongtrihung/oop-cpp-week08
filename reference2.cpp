af301b49ac6cd9dd09db143c138de7b2159d8800
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-18 09:55:46 +0700

    Swap values without using tham chieu

--- reference2.cpp
+++ reference2.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void swapValues (int& x, int& y){
-    int temp = x;
-    x = y;
-    y = temp;
-}
+int a = 5;
+int b = 10;
 
+void swapValues(){
+    int temp = a;
+    a = b;
+    b = temp;
+}
 int main(){
-    int a = 5;
-    int b = 10;
     cout << "Before: a = " << a << ", b = " << b << endl;
-    swapValues(a, b);
+
+    swapValues();
+    
     cout << "After: a = " << a << ", b = " << b << endl;
 
     return 0;
