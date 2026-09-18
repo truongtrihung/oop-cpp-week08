96c46df091849dde051d3b97e51baedbc7b6630f
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-18 10:17:16 +0700

    Swap using references

--- reference4.cpp
+++ reference4.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void addFive(int &x){
-    x += 5;
+void swapValues (int& x, int& y){
+    int temp = x;
+    x = y; 
+    y = temp;
 }
 
 int main(){
-    int a = 10;
-    addFive(a);
-    cout << a << endl;
-    // expect 15
+    int a = 4, b = 9;
+    swapValues(a, b);
+    cout << a << " " << b << endl;
+    // Ouput: a = 9, b = 4
     return 0;
 }
