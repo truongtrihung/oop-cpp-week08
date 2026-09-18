35d513425a069b379c44a15a6c3def717673e184
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-18 10:08:58 +0700

    Reference 3 (try variation)

--- reference3.cpp
+++ reference3.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 int main(){
-    int a = 5;
-    int& r = a;     // r is reference to a
-    r = 10;         // modify through reference
-    cout << a << endl;  // (1)
-    cout << r << endl;  // (2)
+    int x = 3;
+    
+    int& y = x;      // Khai báo y là tham chiếu (reference) của x. 
+                     // y đóng vai trò là "biệt danh" của x, cả x và y cùng quản lý chung 1 vùng nhớ.
 
-    return 0;
+    x = 7;           // Đổi giá trị của x thành 7. 
+                     // Vì y dùng chung vùng nhớ với x nên giá trị khi truy cập qua y lúc này cũng là 7.
+
+    y = 2;           // Gán giá trị 2 thông qua biến y. 
+                     // Vùng nhớ chung bị cập nhật thành 2, kéo theo giá trị của x cũng chuyển thành 2.
+
+    cout << x << " " << y << endl; // In giá trị của x và y ra màn hình (Kết quả in ra: 2 2)
+    
+    return 0;        
 }
