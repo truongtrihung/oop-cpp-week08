63fdbd04ae209aba1b1d11af9050c27d9c8eeea3
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-22 17:12:34 +0700

    Swap Two Strings

--- reference7.cpp
+++ reference7.cpp
@@ -2,14 +2,16 @@
 #include <string>
 using namespace std;
 
-void addExclaim (string& str){
-    str += "!!";
+void swapStr (string& a, string& b){
+    string temp = a;
+    a = b;
+    b = temp;
 }
 
 int main(){
-    string msg = "Good Day";
-    addExclaim(msg);
-    cout << msg << endl;
-
+    string x = "cat";
+    string y = "dog";
+    swapStr(x ,y);
+    cout << x << " " << y << endl;
     return 0;
 }
