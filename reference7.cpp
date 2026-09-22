9eaa6f5687afeca443c6994988df0130587995fe
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-22 17:20:05 +0700

    Reference vs Copy

--- reference7.cpp
+++ reference7.cpp
@@ -2,14 +2,18 @@
 #include <string>
 using namespace std;
 
-void changeFirstChar (string& str){
-    str[0] = ']';
-}
 
 int main(){
-    string name = "Python";
-    changeFirstChar(name);
-    cout << name << endl;
-    
+    string s = "Hello";
+    string r = s;       // r is copy of s
+    string& ref = s;    // ref is a reference to s
+
+    r += "!!!";         // modify the copy
+    ref += "???";       // modify the original
+
+    cout << "s    :" << s << endl;
+    cout << "r    :" << r << endl;
+    cout << "ref  :" << ref << endl;
+
     return 0;
 }
